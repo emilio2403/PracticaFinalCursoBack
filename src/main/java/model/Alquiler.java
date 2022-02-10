cdaa034377b850162baa0d1f8e03e8f3c580edc3
Eneko <eneko12rebollo@gmail.com>
2022-02-10 10:22:15 +0100

    inicio de mis modelo

--- src/main/java/model/Alquiler.java
+++ src/main/java/model/Alquiler.java
@@ -1,4 +1,57 @@
 package model;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Alquiler {
+
+    private long id;
+    private LocalDateTime inicio;
+    private LocalDateTime fin;
+    private double coste;
+    private Infraestructura infraestructura;
+
+    @Id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(LocalDateTime inicio) {
+        this.inicio = inicio;
+    }
+
+    public LocalDateTime getFin() {
+        return fin;
+    }
+
+    public void setFin(LocalDateTime fin) {
+        this.fin = fin;
+    }
+
+    public double getCoste() {
+        return coste;
+    }
+
+    public void setCoste(double coste) {
+        this.coste = coste;
+    }
+
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "alquileres")
+    public Infraestructura getInfraestructura() {
+        return infraestructura;
+    }
+
+    public void setInfraestructura(Infraestructura infraestructura) {
+        this.infraestructura = infraestructura;
+    }
 }
--- src/main/java/model/Infraestructura.java
+++ src/main/java/model/Infraestructura.java
@@ -1,4 +1,31 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
 public class Infraestructura {
+
+    private long id;
+    private List<Alquiler> alquileres;
+
+    @Id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @OneToMany
+    public List<Alquiler> getAlquileres() {
+        return alquileres;
+    }
+
+    public void setAlquileres(List<Alquiler> alquileres) {
+        this.alquileres = alquileres;
+    }
 }
