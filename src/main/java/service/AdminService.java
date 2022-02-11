9f476b7aaffe570a71e569338b5d16fd5b2cfd7b
Eneko <eneko12rebollo@gmail.com>
2022-02-11 11:58:43 +0100

    service base

---
+++ src/main/java/service/AdminService.java
@@ -0,0 +1,15 @@
+package service;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    private final AdminService service;
+
+
+}
---
+++ src/main/java/service/AlquilerService.java
@@ -0,0 +1,11 @@
+package service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AlquilerService {
+
+    private final AlquilerService service;
+}
