846fad82e450f0c8cb3b0f6fd0f3347b5e133591
Dylan Hurtado López <dylanhurtado43@gmail.com>
2022-02-11 11:43:51 +0100

    Client Repository added

--- src/main/java/repository/ClienteRepository.java
+++ src/main/java/repository/ClienteRepository.java
@@ -1,4 +1,8 @@
 package repository;
 
-public interface ClienteRepository {
+import model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+    Cliente findByCorreo(String correo);
 }
