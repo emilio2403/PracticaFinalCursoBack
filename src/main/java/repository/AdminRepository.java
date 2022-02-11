24930c2a68ec471400eec62243a1aa886c797832
Eneko <eneko12rebollo@gmail.com>
2022-02-11 11:21:16 +0100

    repository de infraestructura

--- src/main/java/repository/AdminRepository.java
+++ src/main/java/repository/AdminRepository.java
@@ -1,4 +1,4 @@
 package repository;
 
-public class AdminRepository {
+public interface AdminRepository {
 }
---
+++ src/main/java/repository/InfraestructuraRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import model.Infraestructura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InfraestructuraRepository extends JpaRepository<Infraestructura, Long> {
+    List<Infraestructura> findAllByTipo(String tipo);
+}
