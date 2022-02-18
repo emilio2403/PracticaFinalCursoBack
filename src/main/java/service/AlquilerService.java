4911ffc264ae79e3c63b7731071c7d702b3822a3
Eneko <eneko12rebollo@gmail.com>
2022-02-18 11:10:53 +0100

    service Infraestuctura

--- src/main/java/service/AlquilerService.java
+++ src/main/java/service/AlquilerService.java
@@ -2,10 +2,11 @@ package service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import repository.AlquilerRepository;
 
 @Service
 @RequiredArgsConstructor
 public class AlquilerService {
 
-    private final AlquilerService service;
+    private final AlquilerRepository repository;
 }
