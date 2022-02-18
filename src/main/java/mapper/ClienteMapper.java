e6d2c22b356b430525532751da8c0d5c2ee0b2f3
Eneko <eneko12rebollo@gmail.com>
2022-02-18 12:03:10 +0100

    inicio mappers

--- src/main/java/mapper/ClienteMapper.java
+++ src/main/java/mapper/ClienteMapper.java
@@ -1,4 +1,20 @@
 package mapper;
 
+import dto.ClienteDTO;
+import lombok.RequiredArgsConstructor;
+import model.Cliente;
+import org.modelmapper.ModelMapper;
+
+@RequiredArgsConstructor
 public class ClienteMapper {
+
+    private final ModelMapper mapper;
+
+    public ClienteDTO toDTO(Cliente cliente){
+        return mapper.map(cliente,ClienteDTO.class);
+    }
+
+    public Cliente toModel(ClienteDTO dto){
+        return mapper.map(dto, Cliente.class);
+    }
 }
