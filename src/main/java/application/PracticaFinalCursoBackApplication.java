62f6be3435df4b32650b17872cbd886460b25385
Eneko <eneko12rebollo@gmail.com>
2022-02-23 14:12:57 +0100

    admins al bean

--- src/main/java/application/PracticaFinalCursoBackApplication.java
+++ src/main/java/application/PracticaFinalCursoBackApplication.java
@@ -1,14 +1,12 @@
 package application;
 
 import application.model.Admin;
-import application.repository.AdminRepository;
-
 import application.model.Cliente;
 import application.model.Infraestructura;
 import application.model.TipoPista;
+import application.repository.AdminRepository;
 import application.repository.ClienteRepository;
 import application.repository.InfraestructuraRepository;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,31 +25,31 @@ public class PracticaFinalCursoBackApplication {
     }
 
     @Bean
-    public CommandLineRunner initDatos(InfraestructuraRepository repository, ClienteRepository clienteRepository,
-                                      AdminRepository repository) {
+    public CommandLineRunner initDatos(InfraestructuraRepository infraestructuraRepository, ClienteRepository clienteRepository,
+                                       AdminRepository adminRepository) {
         return (args) -> {
-            repository.deleteAll();
+            infraestructuraRepository.deleteAll();
             clienteRepository.deleteAll();
-            repository.deleteAll();
-            
-            repository.save(new Infraestructura(new ArrayList<>(), TipoPista.BALONCESTO.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
-            repository.save(new Infraestructura(new ArrayList<>(), TipoPista.TENIS.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
-            repository.save(new Infraestructura(new ArrayList<>(), TipoPista.VOLEY.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
-            repository.save(new Infraestructura(new ArrayList<>(), TipoPista.FUTBOL.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
-            repository.save(new Infraestructura(new ArrayList<>(), TipoPista.FUTBOLSALA.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
-            repository.save(new Infraestructura(new ArrayList<>(), TipoPista.PADEL.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
+            adminRepository.deleteAll();
+
+            infraestructuraRepository.save(new Infraestructura(new ArrayList<>(), TipoPista.BALONCESTO.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
+            infraestructuraRepository.save(new Infraestructura(new ArrayList<>(), TipoPista.TENIS.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
+            infraestructuraRepository.save(new Infraestructura(new ArrayList<>(), TipoPista.VOLEY.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
+            infraestructuraRepository.save(new Infraestructura(new ArrayList<>(), TipoPista.FUTBOL.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
+            infraestructuraRepository.save(new Infraestructura(new ArrayList<>(), TipoPista.FUTBOLSALA.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
+            infraestructuraRepository.save(new Infraestructura(new ArrayList<>(), TipoPista.PADEL.name(), "foto", LocalDateTime.now(), LocalDateTime.now(), "descripcion"));
 
             clienteRepository.save(new Cliente("Ansufati", "ansufati@gmail.com", "contraseña", "fotaza", new ArrayList<>()));
             clienteRepository.save(new Cliente("Obama", "obama@gmail.com", "contraseña", "fotaza", new ArrayList<>()));
             clienteRepository.save(new Cliente("Greta Thunberg", "salvaanimales@gmail.com", "contraseña", "fotaza", new ArrayList<>()));
             clienteRepository.save(new Cliente("Vinicius", "vinicius@gmail.com", "contraseña", "fotaza", new ArrayList<>()));
-            
-            
-            repository.insert(new Admin("Emilio", "emilio@gmail.com", "admin", "url"));
-            repository.insert(new Admin("Neko", "neko@gmail.com", "admin", "url"));
-            repository.insert(new Admin("Dylan", "dylan@gmail.com", "admin", "url"));
-            repository.insert(new Admin("Saul", "saul@gmail.com", "admin", "url"));
-            repository.insert(new Admin("Loli", "loli@gmail.com", "admin", "url"));
+
+
+            adminRepository.insert(new Admin("Emilio", "emilio@gmail.com", "admin", "url"));
+            adminRepository.insert(new Admin("Neko", "neko@gmail.com", "admin", "url"));
+            adminRepository.insert(new Admin("Dylan", "dylan@gmail.com", "admin", "url"));
+            adminRepository.insert(new Admin("Saul", "saul@gmail.com", "admin", "url"));
+            adminRepository.insert(new Admin("Loli", "loli@gmail.com", "admin", "url"));
         };
     }
 }
