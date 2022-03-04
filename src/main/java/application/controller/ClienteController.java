aa0071fdab96b59774a0de945fc0edee84461895
Daniel Rodríguez Muñoz <daniel.rodriguez@alumno.iesluisvives.org>
2022-03-04 23:20:18 +0100

    ClienteController cohesionado.

--- src/main/java/application/controller/ClienteController.java
+++ src/main/java/application/controller/ClienteController.java
@@ -1,15 +1,20 @@
 package application.controller;
 
 import application.dto.ClienteDTO;
+import application.error.GeneralError;
 import application.mapper.ClienteMapper;
 import application.model.Cliente;
 import application.repository.ClienteRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -21,45 +26,71 @@ public class ClienteController {
     private final ClienteMapper mapper;
 
 
+    @ApiOperation(value = "Get All Cliente", notes = "Devuelve una lista de clientes.")
+    @ApiResponse(code = 200, message = "OK", response = ClienteDTO.class)
     @GetMapping("/")
     public ResponseEntity<List<ClienteDTO>> findAll() {
-        return ResponseEntity.status(HttpStatus.FOUND)
-                .body(mapper.toDTOList(clienteRepository.findAll()));
+        return ResponseEntity.status(HttpStatus.FOUND).body(mapper.toDTOList(clienteRepository.findAll()));
     }
 
+    @ApiOperation(value = "Get Cliente By Id", notes = "Devolverá el cliente en caso de encontrarlo.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = ClienteDTO.class),
+            @ApiResponse(code = 400, message = "BAD_REQUEST", response = GeneralError.class)
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<ClienteDTO> findById(@PathVariable UUID id) {
-        return ResponseEntity.status(HttpStatus.FOUND)
-                .body(mapper.toDTO(clienteRepository.findById(id).orElseThrow()));
+    public ResponseEntity findById(@RequestParam(name = "id", required = true) UUID id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        if (cliente.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new GeneralError());
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(mapper.toDTO(cliente.get()));
+        }
     }
 
+    @ApiOperation(value = "Get Cliente By Correo", notes = "Devolverá el cliente en caso de encontrarlo.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = ClienteDTO.class),
+            @ApiResponse(code = 400, message = "BAD_REQUEST", response = GeneralError.class)
+    })
     @GetMapping("/{correo}")
-    public ResponseEntity<ClienteDTO> getByEmail(@PathVariable String correo) {
-        return ResponseEntity.status(HttpStatus.FOUND)
-                .body(mapper.toDTO(clienteRepository.findByCorreo(correo).orElseThrow()));
+    public ResponseEntity getByEmail(@RequestParam(name = "tipo", required = true) String correo) {
+        Optional<Cliente> cliente = clienteRepository.findByCorreo(correo);
+        if (cliente.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new GeneralError());
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(mapper.toDTO(cliente.get()));
+        }
     }
 
-
+    @ApiOperation(value = "Post Cliente", notes = "Devuelve el cliente que se ha insertado.")
+    @ApiResponse(code = 201, message = "Created", response = ClienteDTO.class)
     @PostMapping("/")
     public ResponseEntity<ClienteDTO> postClient(@RequestBody ClienteDTO clienteDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(mapper.toDTO(clienteRepository.insert(mapper.toModel(clienteDTO))));
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.toDTO(clienteRepository.insert(mapper.toModel(clienteDTO))));
     }
 
+    @ApiOperation(value = "Put Cliente", notes = "Devuelve el cliente que ha sido modificado.")
+    @ApiResponse(code = 200, message = "OK", response = ClienteDTO.class)
     @PutMapping("/")
     public ResponseEntity<ClienteDTO> putClient(@RequestBody ClienteDTO clienteDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(mapper.toDTO(clienteRepository.save(mapper.toModel(clienteDTO))));
+        return ResponseEntity.status(HttpStatus.OK).body(mapper.toDTO(clienteRepository.save(mapper.toModel(clienteDTO))));
     }
 
+    @ApiOperation(value = "Delete Cliente", notes = "Devolverá una respuesta sin cuerpo.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "No Content", response = ClienteDTO.class),
+            @ApiResponse(code = 400, message = "Bad Request", response = GeneralError.class)
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<ClienteDTO> delete(@PathVariable UUID id) {
-        Cliente cliente = clienteRepository.findById(id).orElse(null);
-        if (cliente != null) {
-            clienteRepository.delete(cliente);
-            return ResponseEntity.ok(mapper.toDTO(cliente));
+    public ResponseEntity delete(@RequestParam(name = "id", required = true) UUID id) {
+        clienteRepository.deleteById(id);
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        if (cliente.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new GeneralError());
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ClienteDTO());
     }
 
 
